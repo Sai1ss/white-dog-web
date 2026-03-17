6371c1f83082cf256a98967da89b320376207549
Sails <164034767+Sai1ss@users.noreply.github.com>
2026-03-17 17:38:59 -0300

    Fix: Clean git conflicts and enable media files

--- core/urls.py
+++ core/urls.py
@@ -6,3 +6,6 @@ urlpatterns = [
     # Aquí delegamos todo el tráfico de la portada a la app 'landing'
     path('', include('landing.urls')), 
 ]
+
+if settings.DEBUG or True: # Forzamos para la prueba
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
